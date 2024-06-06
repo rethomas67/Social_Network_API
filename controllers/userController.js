95a60ac3b3f79822589fef478d213a145476d995
rthomas86 <rebkthomas72@gmail.com>
2024-06-05 17:49:51 -0700

    added get user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,11 +16,17 @@ module.exports = {
   },
   async getUser(req, res) {
     try {
-      const users = await User.find();
-      const userObj = {
-        users,
-      };
-      return res.json(userObj);
+      const user = await User.findOne({ _id: req.params.userId })
+        .select("-__v")
+        .lean();
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      res.json({
+        user,
+      });
     } catch (err) {
       console.log(err);
       return res.status(500).json(err);
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
-const { createUser, getUsers } = require("../../controllers/userController");
+const {
+  createUser,
+  getUsers,
+  getUser,
+} = require("../../controllers/userController");
 
 router.route("/").get(getUsers).post(createUser);
+router.route("/:userId").get(getUser);
 
 module.exports = router;
